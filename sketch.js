52b5c65c1deb7afb61344dcae16f53db83bbc6df
Andrei Dimaano <andreiidimaano@gmail.com>
2021-04-04 13:40:06 -0700

    add red nose

--- sketch.js
+++ sketch.js
@@ -1,11 +1,20 @@
 let video;
 let poseNet;
+let pose;
 
 function setup() {
     createCanvas(640, 480);
     video = createCapture(VIDEO);
     video.hide();
-    posetNet = ml5.poseNet(video, modelLoaded);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(poses) {
+    console.log(poses);
+    if(poses.length > 0) {
+        pose = poses[0].pose;
+    }
 }
 
 function modelLoaded() {
@@ -14,4 +23,10 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0);
+
+    if(pose) {
+        fill(255, 0, 0);
+        ellipse(pose.nose.x, pose.nose.y, 64);
+    }
+    
 }
