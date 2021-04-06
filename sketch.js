736e70d8a13fe6a1d067037c471c012e05033062
Andrei Dimaano <andreiidimaano@gmail.com>
2021-04-05 22:02:25 -0700

    finish ml5!

--- sketch.js
+++ sketch.js
@@ -1,6 +1,29 @@
 let video;
 let poseNet;
 let pose;
+let skeleton;
+
+let brain;
+let state = 'waiting';
+let targetLabel;
+let poseLabel = 'Y';
+
+function keyPressed() {
+    if (key == 's') {
+        brain.saveData();
+    } else {
+        targetLabel = key;
+        console.log(targetLabel);
+        setTimeout(function() {
+            console.log('collecting');
+            state = 'collecting';   
+            setTimeout(function() {
+                console.log('not collecting');
+                state = 'waiting';
+            }, 5000); 
+        }, 5000);
+    }
+}
 
 function setup() {
     createCanvas(640, 480);
@@ -8,12 +31,79 @@ function setup() {
     video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
+
+    let options = {
+        inputs: 34,
+        outputs: 4,
+        task: 'classification',
+        debug: true
+    }
+    brain = ml5.neuralNetwork(options);
+    const modelInfo = {
+        model: 'model/model.json',
+        metadata: 'model/model_meta.json',
+        weights: 'model/model.weights.bin',
+      };
+      brain.load(modelInfo, brainLoaded);
+    // brain.loadData('ymca.json', dataReady);
+}
+
+function brainLoaded() {
+    console.log('pose classification ready');
+    classifyPose();
+}
+
+function classifyPose () {
+    if(pose) {
+        let inputs = [];
+        for(let i = 0; i < pose.keypoints.length; i++) {
+            let x = pose.keypoints[i].position.x;
+            let y = pose.keypoints[i].position.y;
+            inputs.push(x);
+            inputs.push(y);
+        }
+    
+        brain.classify(inputs, gotResult);
+    } else {
+        setTimeout(classifyPose, 100);
+    }
+}
+
+function gotResult(error, results) {
+    poseLabel = results[0].label;
+    console.log(results[0].label);
+    classifyPose();
+}
+
+function dataReady() {
+    brain.normalizeData();
+    brain.train({epochs: 50}, finished);
+}
+
+function finished() {
+    console.log('model trained');
+    brain.save();
 }
 
 function gotPoses(poses) {
-    console.log(poses);
+    // console.log(poses);
     if(poses.length > 0) {
         pose = poses[0].pose;
+        skeleton = poses[0].skeleton;
+
+        if(state == 'collecting') {
+            let inputs = [];
+            for(let i = 0; i < pose.keypoints.length; i++) {
+                let x = pose.keypoints[i].position.x;
+                let y = pose.keypoints[i].position.y;
+                inputs.push(x);
+                inputs.push(y);
+            }
+
+            let target = [targetLabel];
+
+            brain.addData(inputs, target);
+        }
     }
 }
 
@@ -22,18 +112,33 @@ function modelLoaded() {
 }
 
 function draw() {
+    push();
     translate(video.width, 0);
     scale(-1,1);
     image(video, 0, 0, video.width, video.height);
 
     if(pose) {
-        fill(255, 0, 0);
-        ellipse(pose.nose.x, pose.nose.y, 64);
+        for(let i = 0; i < skeleton.length; i++) {
+            let a = skeleton[i][0];
+            let b = skeleton[i][1];
+            strokeWeight(2);
+            stroke(0);
 
-        fill(255, 0, 0);
-        ellipse(pose.leftWrist.x, pose.leftWrist.y, 64);
-        fill(255, 0, 0);
-        ellipse(pose.rightWrist.x, pose.rightWrist.y, 64);
+            line(a.position.x, a.position.y, b.position.x, b.position.y);
+        }
+        for(let i = 0; i < pose.keypoints.length; i++) {
+            let x = pose.keypoints[i].position.x;
+            let y = pose.keypoints[i].position.y;
+            fill(0);
+            stroke(255);
+            ellipse(x,y,16,16);
+        }
     }
-    
+    pop();
+
+    fill(255, 0, 255);
+    noStroke();
+    textSize(512);
+    textAlign(CENTER, CENTER);
+    text(poseLabel, width / 2, height / 2);
 }
